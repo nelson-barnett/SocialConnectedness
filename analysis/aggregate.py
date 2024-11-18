428999f2b6e46712d1cef46f5d8904e8f34f13c2
Nelson Barnett <nbarnett@mghihp.edu>
2024-11-18 14:33:33 -0500

    Improve aggregation

--- analysis/aggregate.py
+++ analysis/aggregate.py
@@ -22,8 +22,9 @@ for spath in out_root.glob("*"):
     if not spath.is_dir():
         continue
     survey_name = survey_key[spath.name]["name"]
-    this_df = []  # Reset each survey
+    agg_df = []  # Reset aggregate dataframe every new survey
     for fpath in spath.glob("*.csv"):
+        # Collect metadata
         file = fpath.stem
         us_ind = file.find("_")
         sp_ind = file.find(" ")
@@ -32,18 +33,52 @@ for spath in out_root.glob("*"):
         date = file[us_ind + 1 : sp_ind]
         time = file[sp_ind + 1 : file.find("+")]
 
+        # Load file
+        this_df = pd.read_csv(fpath, na_filter=False)
+
+        # Establish sum
         if fpath.stem.endswith("PARSE_ERR"):
             sum_field = "PARSING ERROR"
         elif fpath.stem.endswith("SKIPPED_ANS"):
             sum_field = "SKIPPED ANSWER"
         else:
-            score_df = pd.read_csv(fpath, usecols=["score"])
-            sum_field = score_df.score.sum()
-        this_df.append([subject_id, date, time, sum_field])
+            sum_field = this_df.score.sum()
 
-    aggs[survey_name] = pd.DataFrame(
-        this_df, columns=["Subject ID", "date", "time", "sum"]
-    )
+        # Add this survey's data to aggregate "dataframe" (list, really)
+        # Get subscores if ALSFRS
+        if "ALSFRS" in survey_name:
+            agg_df.append(
+                [subject_id, date, time]
+                + this_df.score.to_list()
+                + [
+                    this_df.score[0:3].sum(),
+                    this_df.score[3:6].sum(),
+                    this_df.score[6:9].sum(),
+                    this_df.score[9:12].sum(),
+                ]
+                + [sum_field]
+            )
+        else:
+            agg_df.append(
+                [subject_id, date, time] + this_df.score.to_list() + [sum_field]
+            )
+
+    # Create column headers (add subscores for ALSFRS)
+    if "ALSFRS" in survey_name:
+        cols = (
+            ["Subject ID", "date", "time"]
+            + this_df["question text"].to_list()
+            + ["Bulbar", "Fine Motor", "Gross Motor", "Respiratory", "sum"]
+        )
+    else:
+        cols = (
+            ["Subject ID", "date", "time"]
+            + this_df["question text"].to_list()
+            + ["sum"]
+        )
+
+    # Key = readable survey name, value = dataframe of scores for every instance of this survey
+    aggs[survey_name] = pd.DataFrame(agg_df, columns=cols)
 
 # Loop through aggregated dataframes and save to separate sheets
 with pd.ExcelWriter(out_root.joinpath("SUMMARY_SHEET.xlsx")) as writer:
