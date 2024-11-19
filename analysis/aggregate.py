f18ae1d6f7018f0910102e2b584e4cdf73db3dbb
Nelson Barnett <nbarnett@mghihp.edu>
2024-11-19 11:29:37 -0500

    Add summary/stats tab to summary sheet

--- analysis/aggregate.py
+++ analysis/aggregate.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 from pathlib import Path
+import statistics
 
 out_root = Path("L:/Research Project Current/Social Connectedness/Nelson/dev/results")
 
@@ -18,6 +19,7 @@ survey_key = (
 
 # Aggregate
 aggs = {}
+summary = {}
 for spath in out_root.glob("*"):
     if not spath.is_dir():
         continue
@@ -63,6 +65,15 @@ for spath in out_root.glob("*"):
                 [subject_id, date, time] + this_df.score.to_list() + [sum_field]
             )
 
+        if not isinstance(sum_field, str):
+            if subject_id in summary.keys():
+                if spath.name in summary[subject_id].keys():
+                    summary[subject_id][spath.name].append(this_df.score.sum())
+                else:
+                    summary[subject_id][spath.name] = [this_df.score.sum()]
+            else:
+                summary[subject_id] = {spath.name: [this_df.score.sum()]}
+
     # Create column headers (add subscores for ALSFRS)
     if "ALSFRS" in survey_name:
         cols = (
@@ -80,7 +91,35 @@ for spath in out_root.glob("*"):
     # Key = readable survey name, value = dataframe of scores for every instance of this survey
     aggs[survey_name] = pd.DataFrame(agg_df, columns=cols)
 
+
+subj_ids = []
+surv_names = []
+n = []
+avgs = []
+stds = []
+for s_id, survey_dicts in summary.items():
+    for survey_id, survey_sum in survey_dicts.items():
+        subj_ids.append(s_id)
+        surv_names.append(survey_key[survey_id]["name"])
+        n.append(len(survey_sum))
+        avgs.append(statistics.fmean(survey_sum))
+        if len(survey_sum) > 1:
+            stds.append(statistics.stdev(survey_sum))
+        else:
+            stds.append(float("nan"))
+
+summary_df = pd.DataFrame(
+    {
+        "Subject ID": subj_ids,
+        "Survey Name": surv_names,
+        "n": n,
+        "Mean": avgs,
+        "STD": stds,
+    }
+)
+
 # Loop through aggregated dataframes and save to separate sheets
 with pd.ExcelWriter(out_root.joinpath("SUMMARY_SHEET.xlsx")) as writer:
+    summary_df.to_excel(writer, sheet_name="Summary", index=False)
     for name, df in aggs.items():
         df.to_excel(writer, sheet_name=name, index=False)
