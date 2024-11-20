1eb32dd0abd0e6b35da52e18e3dd9bed026546ee
Nelson Barnett <nbarnett@mghihp.edu>
2024-11-20 10:13:43 -0500

    Use argparse for cli, other touchups

--- analysis/main.py
+++ analysis/main.py
@@ -2,25 +2,24 @@ from parsing import parse, load_key
 from pathlib import Path
 import statistics
 import pandas as pd
-import sys
-
-MIRROR_STRUCTURE = False
+import warnings
+import inspect
+import argparse
 
 DATA_DIR = "L:/Research Project Current/Social Connectedness/Nelson/dev"
 OUT_ROOT = "L:/Research Project Current/Social Connectedness/Nelson/dev/results"
 KEY_PATH = "L:/Research Project Current/Social Connectedness/Nelson/dev/survey_key.csv"
 
 
-def process(data_dir, out_root, key_path, mirror_structure):
+def process(data_dir, out_root, key_path, subject_id="", survey_id=""):
+    # def process(args):
     """Create a cleaned and scored copy of all survey CSVs in `data_dir`
-    saved in `out_root` either by survey ID (`mirror_structure` == False)
-    or in exactly the same layout as `data_dir` (`mirror_structure` == True)
+    saved in `out_root` by survey ID
 
     Args:
         data_dir (str): Path to root directory where data is stored
         out_root (str): Path to directory in which data will be saved
         key_path (str): Path to CSV key containing survey scoring rules
-        mirror_structure (bool): Flag to either mirror structure of `data_dir` or save by survey ID
 
     Raises:
         Exception: KeyError if survey ID does not exist in the provided key
@@ -35,26 +34,22 @@ def process(data_dir, out_root, key_path, mirror_structure):
         except KeyError:
             raise Exception("Survey ID not found in key.")
 
-        if this_key["index"] is None and this_key["invert"] is None:
+        this_subj_id = file.parent.parent.parent.name
+
+        if (
+            (this_key["index"] is None and this_key["invert"] is None)
+            or (subject_id and this_subj_id != subject_id)
+            or (survey_id and file.parent.name != survey_id)
+        ):
             continue
 
-        if mirror_structure:
-            out_dir = out_root.joinpath(
-                file.parent.relative_to(data_dir)
-            )  # Mirror path in results directory
-            prefix = ""
-        else:
-            # Group by survey id
-            out_dir = out_root.joinpath(file.parent.name)
-            prefix = file.parent.parent.parent.name
-
+        out_dir = out_root.joinpath(file.parent.name)
         out_dir.mkdir(exist_ok=True, parents=True)
-        parse(file, out_dir, this_key, prefix)
+        parse(file, out_dir, this_key, this_subj_id)
 
 
 def aggregate(results_dir, key_path):
     """Take all processed data and create a summary sheet saved to `results_dir`.
-    Requires data be saved by survey ID (process called with mirror_structure = False)
 
     Args:
         results_dir (str): Path to directory in which both `processed` data exists and summary sheet will be saved
@@ -81,6 +76,10 @@ def aggregate(results_dir, key_path):
             date = file[us_ind + 1 : sp_ind]
             time = file[sp_ind + 1 : file.find("+")]
 
+            # Parse datetime
+            # dt = file[us_ind + 1 : file.find("+")]
+            # datetime.strptime(dt, "%Y-%m-%d %H_%M_%S")
+            
             # Load file
             this_df = pd.read_csv(fpath, na_filter=False)
 
@@ -95,7 +94,7 @@ def aggregate(results_dir, key_path):
             # Add this survey's data to aggregate "dataframe" (list, really)
             # Get subscores if ALSFRS
             if "ALSFRS" in survey_name:
-                agg_df.append(
+                res = (
                     [subject_id, date, time]
                     + this_df.score.to_list()
                     + [
@@ -105,6 +104,13 @@ def aggregate(results_dir, key_path):
                         this_df.score[9:12].sum(),
                     ]
                     + [sum_field]
+                )
+                # Replace erroring subscores with nan
+                agg_df.append(
+                    [
+                        float("nan") if not isinstance(x, str) and x < 0 else x
+                        for x in res
+                    ]
                 )
             else:
                 agg_df.append(
@@ -172,30 +178,101 @@ def aggregate(results_dir, key_path):
         for name, df in aggs.items():
             df.to_excel(writer, sheet_name=name, index=False)
 
+
+def update(data_dir, out_root, key_path, subject_id="", survey_id=""):
+    if not subject_id and not survey_id:
+        warnings.warn("No subject_id or survey_id specified. Processing and aggregating all data")
+
+    process(data_dir, out_root, key_path, subject_id=subject_id, survey_id=survey_id)
+    aggregate(out_root, key_path)
+
+
+def call_function_with_args(func, args):
+    """
+    Call the function with the arguments extracted from the argparse.Namespace object.
+    FROM: https://gist.github.com/amarao/36327a6f77b86b90c2bca72ba03c9d3a
+
+    Args:
+        func: The function to call.
+        args: The argparse.Namespace object containing the arguments.
+
+    Returns:
+        Any: The result of the function call.
+
+    Author:
+        Laurent DECLERCQ, AGON PARTNERS INNOVATION <l.declercq@konzeptplus.ch>
+    """
+    # Let's inspect the signature of the function so that we can call it with the correct arguments.
+    # We make use of the inspect module to get the function signature.
+    signature = inspect.signature(func)
+
+    # Get the parameters of the function using a dictionary comprehension.
+    # Note: Could be enhanced to handle edge cases (default values, *args, **kwargs, etc.)
+    args = {parameter: getattr(args, parameter) for parameter in signature.parameters}
+
+    # Type cast the arguments to the correct type according to the function signature. We use the annotation of the
+    # parameter to cast the argument. If the annotation is empty, we keep the argument as is. We only process the
+    # arguments that are in the function signature.
+    args = {
+        parameter: (
+            signature.parameters[parameter].annotation(args[parameter])
+            if signature.parameters[parameter].annotation is not inspect.Parameter.empty
+            else args[parameter]
+        )
+        for parameter in args
+        if parameter in signature.parameters
+    }
+
+    # Call the function with the arguments and return the result if any.
+    return func(**args)
+
+def cli():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    # Process
+    parser_process = subparsers.add_parser("process")
+    parser_process.add_argument(
+        "-d", "--data_dir", type=str, nargs="?", default=DATA_DIR
+    )
+    parser_process.add_argument(
+        "-o", "--out_root", type=str, nargs="?", default=OUT_ROOT
+    )
+    parser_process.add_argument(
+        "-k", "--key_path", type=str, nargs="?", default=KEY_PATH
+    )
+    parser_process.add_argument("--subject_id", type=str, nargs="?", default="")
+    parser_process.add_argument("--survey_id", type=str, nargs="?", default="")
+    parser_process.set_defaults(func=process)
+
+    # Aggregate
+    parser_agg = subparsers.add_parser("aggregate")
+    parser_agg.add_argument("-d", "--results_dir", type=str, default=OUT_ROOT)
+    parser_agg.add_argument("-k", "--key_path", type=str, default=KEY_PATH)
+    parser_agg.set_defaults(func=aggregate)
+
+    # Update
+    parser_update = subparsers.add_parser("update")
+    parser_update.add_argument("-d", "--data_dir", type=str, default=DATA_DIR)
+    parser_update.add_argument("-o", "--out_root", type=str, default=OUT_ROOT)
+    parser_update.add_argument("-k", "--key_path", type=str, default=KEY_PATH)
+    parser_update.add_argument("--subject_id", type=str, default="")
+    parser_update.add_argument("--survey_id", type=str, default="")
+    parser_update.set_defaults(func=update)
+
+    # Collect args
+    args = parser.parse_args()
+    
+    # Print info
+    print("Running", args.func.__name__, "with arguments:")
+    for arg_name, value in vars(args).items():
+        if arg_name != "func":
+            print(arg_name, ": ", value, sep="")
+    
+    # Call
+    call_function_with_args(args.func, args)
+
 ########### CLI ###########
 if __name__ == "__main__":
-    args = sys.argv
-    if len(args) < 3 and args[1] == "process":
-        print(
-            "Processing surveys using default arguments:\ndata_dir = ",
-            DATA_DIR,
-            "\nout_dir = ",
-            OUT_ROOT,
-            "\nkey = ",
-            KEY_PATH,
-            "\nmirror_path = ",
-            MIRROR_STRUCTURE,
-        )
-        process(DATA_DIR, OUT_ROOT, KEY_PATH, MIRROR_STRUCTURE)
-    elif len(args) < 3 and args[1] == "aggregate":
-        print(
-            "Aggregating using default arguments:\nout_dir = ",
-            OUT_ROOT,
-            "\nkey = ",
-            KEY_PATH,
-        )
-        aggregate(OUT_ROOT, KEY_PATH)
-    else:
-        print("Running ", args[1], " function with supplied arguments")
-        globals()[args[1]](*args[2:])
+    cli()
     print("Complete!")
